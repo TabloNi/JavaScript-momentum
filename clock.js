a0e7be4407fc255d21138829f1610b599fa7aa8b
TabloNi <tabloni0719@gmail.com>
2021-01-03 15:04:57 +0900

    Finish : make 0 to front time

--- clock.js
+++ clock.js
@@ -3,14 +3,15 @@ const clockTitle = clockContainer.querySelector("h1")
 
 function getTime(){
     const date = new Date()
-    const minute = date.getMinutes()
-    const hour = date.getHours()
-    clockTitle.innerHTML = `${hour}:${minute}`
+    const minutes = date.getMinutes()
+    const hours = date.getHours()
+    const seconds = date.getSeconds()
+    clockTitle.innerHTML = `${hours<10 ? `0${hours}` : `${hours}`}:${minutes<10 ? `0${minutes}` : `${minutes}`}:${seconds < 10 ? `0${seconds}`:`${seconds}`}`
 }
 
 function init(){
-
+getTime()
+setInterval(getTime,1)
 
 }
-getTime()
 init()
